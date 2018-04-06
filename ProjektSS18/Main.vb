e35e101acf9f82e5af7b0abf7340150f6672ffd3
Lukas Viebrock <lukas.viebrock96@web.de>
2018-04-06 11:02:25 +0200

    kleine Änderung im Hauptmenü

--- ProjektSS18/Main.vb
+++ ProjektSS18/Main.vb
@@ -1,19 +1,19 @@
 ﻿Public Class Main
 
     'Navigation zu Zeichnungserstellung
-    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+    Private Sub btnCreate_Click() Handles btnCreate.Click
         Exporter.Show()
         Me.Hide()
     End Sub
 
     'Navigation zum Anordnen
-    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+    Private Sub btnSort_Click() Handles btnSort.Click
         Nesting.Show()
         Me.Hide()
     End Sub
 
     'Anleitung öffnen
-    Private Sub btnInstructions_Click(sender As Object, e As EventArgs) Handles btnInstructions.Click
+    Private Sub btnInstructions_Click() Handles btnInstructions.Click
         Dim myTempFile As String = IO.Path.Combine(IO.Path.GetTempPath, "Anleitung.pdf")
         'Resource als Datei im Temp-Ordner speichern
         My.Computer.FileSystem.WriteAllBytes(myTempFile, My.Resources.Anleitung, False)
